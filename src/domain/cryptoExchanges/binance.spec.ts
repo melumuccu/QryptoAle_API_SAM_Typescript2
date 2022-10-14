af1295456dd31102d00f5746ba3689ab3fb69a49
Fujisawa Koki <melumuccu413@gmail.com>
2022-10-14 16:37:53 +0900

    Test: (RED)Binance#fetchBalances 保有数量0のassetを除外
    ・現状では保有数量0の全てのassetの情報を返しているが、保有していないassetは無視して良いためフィルタリングする

--- src/domain/cryptoExchanges/binance.spec.ts
+++ src/domain/cryptoExchanges/binance.spec.ts
@@ -1,3 +1,4 @@
+import { Account, TradingType } from 'binance-api-node';
 import * as dotenv from 'dotenv';
 import { CryptoExchangesConsts } from '../../consts/cryptoExchangesConsts';
 import { MyBinance } from './binance';
@@ -28,11 +29,26 @@ describe('CalculateService', () => {
     expect(binance).toBeTruthy();
   });
 
-  test('#fetchBalances Balancesが取得できているか', async () => {
+  test('#fetchBalances 正常に動作しているか', async () => {
     const target = await binance.fetchBalances();
     expect(target['USDT']).toEqual({
       free: expect.any(String),
       locked: expect.any(String),
+    });
+  });
+
+  test('#fetchBalances 保有数量0のassetを除外できているか', async () => {
+    const spy = jest.spyOn(binance.sdk, 'accountInfo').mockImplementation(() => {
+      return new Promise(resolve => {
+        resolve(account1);
+      });
+    });
+    const target = await binance.fetchBalances();
+    expect(target).toEqual({
+      USDT: {
+        free: '50',
+        locked: '500',
+      },
     });
   });
 
@@ -46,3 +62,28 @@ describe('CalculateService', () => {
     expect(target).toEqual(expect.any(Array));
   });
 });
+
+const account1: Account = {
+  balances: [
+    {
+      asset: 'USDT',
+      free: '50',
+      locked: '500',
+    },
+    {
+      asset: 'BTC',
+      free: '0.0000000000',
+      locked: '0',
+    },
+  ],
+  accountType: TradingType.MARGIN,
+  buyerCommission: 0,
+  canDeposit: false,
+  canTrade: false,
+  canWithdraw: false,
+  makerCommission: 0,
+  permissions: [],
+  sellerCommission: 0,
+  takerCommission: 0,
+  updateTime: 0,
+};
