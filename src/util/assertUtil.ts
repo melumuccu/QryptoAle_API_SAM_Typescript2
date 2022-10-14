1e2f415844c19c4b8ef603df2ec2246a69330f5f
Fujisawa Koki <melumuccu413@gmail.com>
2022-10-14 18:20:32 +0900

    Feature: AssertUtil#promiseSettledResultFilter

--- src/util/assertUtil.ts
+++ src/util/assertUtil.ts
@@ -12,4 +12,20 @@ export class AssertUtil {
   filterRejected<T>(x: PromiseSettledResult<T>): x is PromiseRejectedResult {
     return x.status === 'rejected';
   }
+
+  /**
+   * PromiseSettledResultを処理する
+   *
+   * ・fulfilledのものだけを返す
+   * ・rejectedのものは汎用的なエラーログを吐く
+   *
+   * @param x PromiseSettledResult<T>[]
+   * @returns T[]
+   */
+  promiseSettledResultFilter<T>(x: PromiseSettledResult<T>[]) {
+    x.filter(this.filterRejected).forEach(y => {
+      console.debug(`rejected reason: ${y.reason}`);
+    });
+    return x.filter(this.filterFulfilled).map(x => x.value);
+  }
 }
