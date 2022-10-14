456192336da3b68fb86ff624d2ca9c843ee48f2b
Fujisawa Koki <melumuccu413@gmail.com>
2022-10-14 18:20:57 +0900

    Docs: コメント微修正(BigNumberUtilはv2で廃止の旨)

--- src/util/bigNumberUtil.ts
+++ src/util/bigNumberUtil.ts
@@ -1,5 +1,6 @@
 import BigNumber from 'bignumber.js';
 
+// MEMO v2以降では使わない。CalculateUtilを使う。
 export class BigNumberUtil {
   BN(numStr: string | number) {
     return new BigNumber(Number(numStr));
