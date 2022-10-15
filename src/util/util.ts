ddd907d7cf80b393513a9c2e549ad6a4f424d5aa
Fujisawa Koki <melumuccu413@gmail.com>
2022-10-15 20:37:47 +0900

    Feature: Util#deepmerge

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,14 +1,17 @@
+import merge from 'ts-deepmerge';
+
 export class Util {
   /** コンストラクタ */
   constructor() {}
 
   /**
    * ObjectをDeepMergeする
+   * ('ts-deepmerge' を使用。https://www.npmjs.com/package/deepmerge)
    *
    * @param objs
    * @returns
    */
   static deepmerge(objs: Object[]): Object {
-    return {};
+    return merge(...objs);
   }
 }
